c0f07cc9e2320ac763db9172250c19bd4a3e9f19
bigelephant29 <dc9491@gmail.com>
2016-07-31 19:23:42 +0800

    move SECRET_KEY to recruitsite/secret.py, and ignore this file

--- recruitsite/settings.py
+++ recruitsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from recruitsite.secret import DJANGO_SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c%gkdt^)+!*-l7((&cljdf9gr21wuf^oe7)h2fezifa96y!vg7'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
